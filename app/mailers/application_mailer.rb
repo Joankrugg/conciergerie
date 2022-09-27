964024baa837b96820f3fa8e5e22d551f4e35a2f
Philippe chambon <joan_krugg@live.fr>
2022-09-27 12:25:36 +0200

    mailer for production try

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "p.chambon@millesime-collection.com"
   layout "mailer"
 end
--- test/mailers/previews/vip_brindos_mailer_preview.rb
+++ test/mailers/previews/vip_brindos_mailer_preview.rb
@@ -1,4 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/vip_brindos_mailer
 class VipBrindosMailerPreview < ActionMailer::Preview
-
+  def creation(brindos_vip)
+    @brindos_vip = brindos_vip # Instance variable => available in view
+    mail(to: @brindos_vip.email, subject: 'Vous souhaitez Être recontacté')
+  end
 end
