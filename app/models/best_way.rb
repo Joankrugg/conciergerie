2f4d666424c81f9905f8c7fede8d9ff647dc90f7
Philippe chambon <joan_krugg@live.fr>
2022-09-28 05:44:26 +0200

    optional: true

--- app/models/best_way.rb
+++ app/models/best_way.rb
@@ -1,6 +1,6 @@
 class BestWay < ApplicationRecord
-  belongs_to :sacy_vip
-  belongs_to :brindos_vip
-  belongs_to :megeve_vip
-  belongs_to :raba_vip
+  belongs_to :sacy_vip, optional: true
+  belongs_to :brindos_vip, optional: true
+  belongs_to :megeve_vip, optional: true
+  belongs_to :raba_vip, optional: true
 end
