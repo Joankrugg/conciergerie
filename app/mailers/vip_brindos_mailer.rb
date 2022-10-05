5f889989a484e58b3697b4c5f34b2b703fb90874
Philippe chambon <joan_krugg@live.fr>
2022-10-05 11:33:15 +0200

    brindos ready

--- app/mailers/vip_brindos_mailer.rb
+++ app/mailers/vip_brindos_mailer.rb
@@ -2,7 +2,7 @@ class VipBrindosMailer < ApplicationMailer
   def creation(brindos_vip)
     destination = 'conciergerie@brindos-cotebasque.com'
     @brindos_vip = brindos_vip # Instance variable => available in view
-    mail(to: destination, subject: 'Un client ayant réservé à Brindos lac et château souhaite être contacté par la conciergerie')
+    mail(to: destination, subject: '[test de développement à ignorer] Un client ayant réservé à Brindos lac et château souhaite être contacté par la conciergerie')
   end
 end
 
