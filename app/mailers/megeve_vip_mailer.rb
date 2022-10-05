f0c82b13f8ea61ecf3b63003d7f957e03b57fb6d
Philippe chambon <joan_krugg@live.fr>
2022-10-05 11:44:39 +0200

    everything is ready

--- app/mailers/megeve_vip_mailer.rb
+++ app/mailers/megeve_vip_mailer.rb
@@ -2,6 +2,6 @@ class MegeveVipMailer < ApplicationMailer
   def creation(megeve_vip)
     destination = 'concierge@lesoleildor-megeve.fr'
     @megeve_vip = megeve_vip # Instance variable => available in view
-    mail(to: destination, subject: "Un client ayant réservé au Soleil d'Or souhaite être contacté par la conciergerie")
+    mail(to: destination, subject: "[test de développement à ignorer] Un client ayant réservé au Soleil d'Or souhaite être contacté par la conciergerie")
   end
 end
